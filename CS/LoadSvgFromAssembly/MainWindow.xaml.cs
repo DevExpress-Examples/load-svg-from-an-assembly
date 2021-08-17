83c39b0653b54a73bda6c7caa53866742637bbdb
Anton Sameishchev <anton.sameishchev@devexpress.com>
2021-08-17 16:11:19 +0300

    Code added

---
+++ CS/LoadSvgFromAssembly/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+using DevExpress.Xpf.Core.Native;
+
+namespace LoadSvgFromAssembly
+{
+    public partial class MainWindow
+    {
+        const string libraryName = @"ImagesLibrary";
+        const string libraryLocation = "data\\ImagesLibrary.dll";
+        const string resourceFileName = @"Logo_Svg_Res";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void LoadImage()
+        {
+            Assembly assembly = Assembly.LoadFrom(libraryLocation);
+            assembly.GetManifestResourceInfo(resourceFileName);
+            System.Resources.ResourceManager resourceManager = new System.Resources.ResourceManager($"{libraryName}.Resources", assembly);
+            object obj = resourceManager.GetObject(resourceFileName, new System.Globalization.CultureInfo("en-US"));
+            byte[] SVGinBytes = (byte[])obj;
+
+            if (SVGinBytes == null)
+                return;
+
+            image.Source = WpfSvgRenderer.CreateImageSource(new MemoryStream(SVGinBytes), 1d, null);
+        }
+
+        void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            //load svg image from library (.dll file)
+            LoadImage();
+        }
+    }
+}
