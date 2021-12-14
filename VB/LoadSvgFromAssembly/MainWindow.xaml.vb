b8226a90a28559c6723833c102befc8e9c2fff52
DevExpressExampleBot <fakeEmail>
2021-12-14 11:36:51 +0300

    Source auto update [skip ci]

--- VB/LoadSvgFromAssembly/MainWindow.xaml.vb
+++ VB/LoadSvgFromAssembly/MainWindow.xaml.vb
@@ -1,35 +1,35 @@
-﻿Imports System.IO
+Imports System.IO
 Imports System.Reflection
 Imports System.Windows
 Imports DevExpress.Xpf.Core.Native
 
 Namespace LoadSvgFromAssembly
-	Partial Public Class MainWindow
-		Private Const libraryName As String = "ImagesLibrary"
-		Private Const libraryLocation As String = "data\ImagesLibrary.dll"
-		Private Const resourceFileName As String = "Logo_Svg_Res"
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class MainWindow
 
-		Private Sub LoadImage()
-			Dim assembly As System.Reflection.Assembly = System.Reflection.Assembly.LoadFrom(libraryLocation)
-			assembly.GetManifestResourceInfo(resourceFileName)
-			Dim resourceManager As New System.Resources.ResourceManager($"{libraryName}.Resources", assembly)
-			Dim obj As Object = resourceManager.GetObject(resourceFileName, New System.Globalization.CultureInfo("en-US"))
-			Dim SVGinBytes() As Byte = DirectCast(obj, Byte())
+        Const libraryName As String = "ImagesLibrary"
 
-			If SVGinBytes Is Nothing Then
-				Return
-			End If
+        Const libraryLocation As String = "data\ImagesLibrary.dll"
 
-			image.Source = WpfSvgRenderer.CreateImageSource(New MemoryStream(SVGinBytes), 1R, Nothing)
-		End Sub
+        Const resourceFileName As String = "Logo_Svg_Res"
 
-		Private Sub Button_OnClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			'load svg image from library (.dll file)
-			LoadImage()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub LoadImage()
+            Dim assembly As Assembly = Assembly.LoadFrom(libraryLocation)
+            assembly.GetManifestResourceInfo(resourceFileName)
+            Dim resourceManager As System.Resources.ResourceManager = New System.Resources.ResourceManager($"{libraryName}.Resources", assembly)
+            Dim obj As Object = resourceManager.GetObject(resourceFileName, New System.Globalization.CultureInfo("en-US"))
+            Dim SVGinBytes As Byte() = CType(obj, Byte())
+            If SVGinBytes Is Nothing Then Return
+            Me.image.Source = WpfSvgRenderer.CreateImageSource(New MemoryStream(SVGinBytes), 1R, Nothing)
+        End Sub
+
+        Private Sub Button_OnClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            'load svg image from library (.dll file)
+            LoadImage()
+        End Sub
+    End Class
 End Namespace
